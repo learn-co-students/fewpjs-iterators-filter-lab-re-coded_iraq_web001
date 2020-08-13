1e630f8e5c85e545f5ae0a92843020b045fa95c1
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-08-13 19:54:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching (arr,string){
+
+  let filter = arr.filter(elem=>{
+    return string.toLowerCase()===elem.toLowerCase();
+  })
+  return filter
+}
+
+function fuzzyMatch(arr,string){
+  let filter = arr.filter(elem=>{
+    return elem.toLowerCase().startsWith(string.toLowerCase())
+  })
+  return filter
+
+}
+function matchName(arr,string){
+
+  let filter = arr.filter(elem=>{
+    return string.toLowerCase() === elem.name.toLowerCase();
+  })
+
+  return filter;
+}
